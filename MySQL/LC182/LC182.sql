7f4cc2e3dbf9f132459f0ffa8d9cf50782d2022a
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-03-27 22:35:32 -0400

    Update LC182.sql

--- MySQL/LC182/LC182.sql
+++ MySQL/LC182/LC182.sql
@@ -11,7 +11,8 @@ INSERT INTO Person182 (id, email) VALUES
 (3, 'a@b.com');
 
 --Write a solution to report all the duplicate emails. Note: Guaranteed no NULL values in the email field
--- return the results in any order
+-- return the results in any order. This is able to work because you don't need a WHERE statement because we don't need to specify the informaiton past selecting all the emails but we need to group them together
+-- and then by using the HAVING COUNT statement we are able to specify which of the emails that we selected before will be brought about in the query. 
 SELECT email
 FROM Person182
 GROUP BY email
